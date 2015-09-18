b687fa89109bb652d5f7e84e7112e2d7f382f4d8
dartiss <dartiss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-18 16:03:58 +0000

    Version 1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-readme-parser/trunk@1248659 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-readme-parser.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Plugin Name: Plugin README Parser
+Plugin URI: https://wordpress.org/plugins/wp-readme-parser/
+Description: Show WordPress Plugin README in your Posts
+Version: 1.3.2
+Author: David Artiss
+Author URI: http://www.artiss.co.uk
+*/
+
+/**
+* Plugin README Parser
+*
+* Main code - include various functions
+*
+* @package	Artiss-README-Parser
+*/
+
+define( 'artiss_readme_parser_version', '1.3.2' );
+
+/**
+* Plugin initialisation
+*
+* Loads the plugin's translated strings
+*
+* @since	1.2
+*/
+
+function arp_plugin_init() {
+
+    $language_dir = plugin_basename( dirname( __FILE__ ) ) . '/languages/';
+
+	load_plugin_textdomain( 'wp-readme_parser', false, $language_dir );
+
+}
+
+add_action( 'init', 'arp_plugin_init' );
+
+/**
+* Main Includes
+*
+* Include all the plugin's functions
+*
+* @since	1.2
+*/
+
+$functions_dir = WP_PLUGIN_DIR . '/wp-readme-parser/includes/';
+
+// Include all the various functions
+
+if ( is_admin() ) {
+
+    include_once( $functions_dir . 'arp-admin-config.php' );			// Various admin. functions
+
+}
+
+include_once( $functions_dir . 'Michelf/MarkdownExtra.inc.php' );		// PHP Markdown Extra
+
+include_once( $functions_dir . 'arp-functions.php' );					// Various functions
+
+include_once( $functions_dir . 'arp-generate-output.php' );				// Generate the output
+?>
